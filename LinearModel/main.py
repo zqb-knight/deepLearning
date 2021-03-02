6c786c3fd471f964bf810dc4e9ffde0e2eacf531
zqb-knight <1065680788@qq.com>
2021-03-02 11:05:27 +0800

    使用pytorch实现线性模型

---
+++ LinearModel/main.py
@@ -0,0 +1,34 @@
+import torch
+
+x_data = torch.Tensor([[1.0], [2.0], [3.0]])
+y_data = torch.Tensor([[2.0], [4.0], [6.0]])
+
+class LinearModel(torch.nn.Module):
+    def __init__(self):
+        super(LinearModel, self).__init__()
+        self.Linear = torch.nn.Linear(1, 1)
+
+    def forward(self, x):
+        y_pred = self.Linear(x)
+        return y_pred
+
+
+model = LinearModel()
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+for epoch in range(1000):
+    y_pred = model.forward(x_data)
+    loss = criterion(y_pred, y_data)
+    print(epoch, loss)
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+print("w=", model.Linear.weight.item())
+print("b=", model.Linear.bias.item())
+
+x_test = torch.Tensor([[4.0]])
+y_test = model(x_test)
+print("After training", y_test.data)
